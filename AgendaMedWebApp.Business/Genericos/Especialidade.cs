e5a8f027e114b4dce5d1adbe344f2e4fad2d0413
Felipe Severo <felipisevero@gmail.com>
2023-10-15 19:09:51 -0300

    Classes especialidade e medicamento

---
+++ AgendaMedWebApp.Business/Genericos/Especialidade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMed.Business.Genericos
+{
+    public class Especialidade
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+
+
+        private static long _currentId = 0;
+        public static List<Especialidade> Especialidades = new List<Especialidade>()
+        {
+            new Especialidade
+            {
+                Nome = "Dermatologista",
+                Codigo = "4040",
+            },
+
+        };
+
+        public Especialidade()
+        {
+            Id = ++_currentId;
+        }
+    }
+}
---
+++ AgendaMedWebApp.Business/Genericos/Medicamento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMed.Business.Genericos
+{
+    public class Medicamento
+    {
+        public long Id { get; set; }       
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Dosagem { get; set; }
+
+
+        private static long _currentId = 0;
+        public static List<Medicamento> Medicamentos = new List<Medicamento>()
+        {
+            new Medicamento
+            {
+                Nome = "Dipirona",
+                Descricao = "Dor de cabeça",
+            },
+        };
+
+        public Medicamento()
+        {
+            Id = ++_currentId;
+        }
+    }
+}
