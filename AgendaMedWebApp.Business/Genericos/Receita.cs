b753f4a6d19246160376629344fe78db3a86e41c
GUSTAVO HENRIQUE DE AZEVEDO STOLF <vendas.stolf@gmail.com>
2023-11-07 21:15:07 -0300

    Pull alterações

--- AgendaMedWebApp.Business/Genericos/Receita.cs
+++ AgendaMedWebApp.Business/Genericos/Receita.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AgendaMedWebApp.Business.Utils;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,38 +13,163 @@ namespace AgendaMedWebApp.Business.Genericos
     {
         public long Id { get; set; }       
         public DateTime DataPrescricao { get; set; }
-        public int Medicamento { get; set; }
+        public int Medicamentos { get; set; }
         public decimal Dosagem { get; set; }
         public int PosologiaHora { get; set; }
         public int PosologiaDias { get; set; }
 
+        public List<ReceitaMedicamentos> _Medicamento { get; set; }  = new List<ReceitaMedicamentos>();
+    
 
 
-        private static long _currentId = 0;
-        public static List<Receita> Receitas = new List<Receita>()
-        {
-            new Receita
+
+        //private static long _currentId = 0;
+        //public static List<Receita> Receitas = new List<Receita>()
+        //{
+        //    new Receita
+        //    {
+        //    DataPrescricao = DateTime.Parse("05/11/2023"),
+        //    Medicamento = 10,
+        //    PosologiaHora = 12,
+        //    PosologiaDias =5,
+        //    Dosagem = 0,           
+        //    },
+        //     new Receita
+        //    {
+        //    DataPrescricao = DateTime.Parse("05/11/2023"),
+        //    Medicamento = 0,
+        //    PosologiaHora = 8,
+        //    PosologiaDias =7,
+        //    Dosagem = 0,
+        //    },
+
+        //};
+
+        //public Receita()
+        //{
+        //    Id = ++_currentId;
+        //}
+      
+    
+        
+        
+
+            public static List<Receita> Read()
             {
-            DataPrescricao = DateTime.Parse("05/11/2023"),
-            Medicamento = 10,
-            PosologiaHora = 12,
-            PosologiaDias =5,
-            Dosagem = 0,           
-            },
-             new Receita
+                var result = new List<Receita>();
+
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT ID, PRESCRIPTION_DATE FROM PRESCRIPTIONS";
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Receita receita = new Receita()
+                        {
+                            Id = reader.GetInt32(0),
+                            DataPrescricao = reader.GetDateTime(1),
+                          
+                        };
+
+                        receita._Medicamento = ReceitaMedicamentos.Read(venda.Id);
+
+                        result.Add(venda);
+                    }
+                }
+
+                return result;
+            }
+
+            public static Venda ReadOne(long id)
             {
-            DataPrescricao = DateTime.Parse("05/11/2023"),
-            Medicamento = 0,
-            PosologiaHora = 8,
-            PosologiaDias =7,
-            Dosagem = 0,
-            },
+                Venda result = null;
 
-        };
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT ID, CLIENTE, VALOR, DATA, VENDEDOR, FORMAPAGAMENTO FROM VENDAS WHERE ID = @ID";
+                    cmd.Parameters.Add(new SqlParameter("@ID", id));
 
-        public Receita()
-        {
-            Id = ++_currentId;
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Venda venda = new Venda()
+                        {
+                            Id = reader.GetInt32(0),
+                            Cliente = reader.GetInt32(1),
+                            Valor = reader.GetDecimal(2),
+                            Data = reader.GetDateTime(3),
+                            Vendedor = reader.GetInt32(4),
+                            FormaPagamento = (FormaPagamento)reader.GetInt32(5),
+                        };
+                        venda.Produtos = VendaProduto.Read(venda.Id);
+
+                        result = venda;
+                    }
+                }
+
+                return result;
+            }
+
+            public void Create()
+            {
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "INSERT INTO VENDAS (CLIENTE, VALOR, DATA, VENDEDOR, FORMAPAGAMENTO)" +
+                                      $"VALUES (@CLIENTE, @VALOR, @DATA, @VENDEDOR, @FORMAPAGAMENTO)";
+
+                    cmd.Parameters.Add(new SqlParameter("@CLIENTE", Cliente));
+                    cmd.Parameters.Add(new SqlParameter("@VALOR", Valor));
+                    cmd.Parameters.Add(new SqlParameter("@DATA", Data));
+                    cmd.Parameters.Add(new SqlParameter("@VENDEDOR", Vendedor));
+                    cmd.Parameters.Add(new SqlParameter("@FORMAPAGAMENTO", FormaPagamento));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public void Update()
+            {
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "UPDATE VENDAS SET CLIENTE = @CLIENTE, VALOR = @VALOR, DATA = @DATA, VENDEDOR = @VENDEDOR, FORMAPAGAMENTO = @FORMAPAGAMENTO WHERE ID = @ID";
+
+                    cmd.Parameters.Add(new SqlParameter("@ID", Id));
+                    cmd.Parameters.Add(new SqlParameter("@CLIENTE", Cliente));
+                    cmd.Parameters.Add(new SqlParameter("@VALOR", Valor));
+                    cmd.Parameters.Add(new SqlParameter("@DATA", Data));
+                    cmd.Parameters.Add(new SqlParameter("@VENDEDOR", Vendedor));
+                    cmd.Parameters.Add(new SqlParameter("@FORMAPAGAMENTO", FormaPagamento));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public void Delete()
+            {
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "DELETE FROM VENDAS WHERE ID = @ID";
+
+                    cmd.Parameters.Add(new SqlParameter("@ID", Id));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public class Medicamento
+            {
+            }
         }
     }
 }
+
---
+++ AgendaMedWebApp.Business/Genericos/ReceitaMedicamentos.cs
@@ -0,0 +1,113 @@
+﻿using AgendaMed.Business.Genericos;
+using AgendaMedWebApp.Business.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMedWebApp.Business.Genericos
+{
+    public class ReceitaMedicamentos
+    {
+
+        public long Id { get; set; }
+        public Medicamento Medication_Id { get; set; }
+        public long Prescription_Id { get; set; }
+        public decimal Dosage { get; set; }
+        public string Prescription { get; set; }
+
+        public static List<ReceitaMedicamentos> Read()
+        {
+            var result = new List<ReceitaMedicamentos>();
+
+            using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT ID, MEDICATION_ID, PRESCRIPTION_ID, DOSAGE, PRESCRIPTION FROM MEDICATION_PRESCRIPTION";
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ReceitaMedicamentos medicamento = new ReceitaMedicamentos()
+                    {
+                        Id = reader.GetInt32(0),
+                        Medication_Id = Medicamento.ReadOne(reader.GetInt32(1)),
+                        Prescription_Id = reader.GetInt32(2),
+                        Dosage = reader.GetInt32(3),
+                        Prescription = reader.GetString(4),
+                    };
+
+                    result.Add(medicamento);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<ReceitaMedicamentos> Read(long Id)
+        {
+            var result = new List<ReceitaMedicamentos>();
+
+            using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT ID, MEDICATION_ID, PRESCRIPTION_ID, DOSAGE, PRESCRIPTION FROM MEDICATION_PRESCRIPTION";
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ReceitaMedicamentos medicamento = new ReceitaMedicamentos()
+                    {
+                        Id = reader.GetInt32(0),
+                        Medication_Id = Medicamento.ReadOne(reader.GetInt32(1)),
+                        Prescription_Id = reader.GetInt32(2),
+                        Dosage = reader.GetInt32(3),
+                        Prescription = reader.GetString(4),
+                    };
+
+                    result.Add(medicamento);
+                }
+            }
+
+            return result;
+        }
+
+            public static ReceitaMedicamentos ReadOne(long id)
+            {
+                ReceitaMedicamentos result = null;
+
+                using (var conn = new SqlConnection(DBConnect.GetDBConnection()))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT ID, MEDICATION_ID, PRESCRIPTION_ID, DOSAGE, PRESCRIPTION FROM MEDICATION_PRESCRIPTION WHERE ID = @ID";
+                    cmd.Parameters.Add(new SqlParameter("@ID", id));
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        ReceitaMedicamentos medicamento = new ReceitaMedicamentos()
+                        {
+                            Id = reader.GetInt32(0),
+                            Medication_Id = Medicamento.ReadOne(reader.GetInt32(1)),
+                            Prescription_Id = reader.GetInt32(2),
+                            Dosage = reader.GetInt32(3),
+                            Prescription = reader.GetString(4),
+                        };
+
+                    result = medicamento;
+                    }
+                }
+
+                return result;
+            }
+
+        }
+    }
+
+
+
